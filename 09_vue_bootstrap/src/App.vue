dc98642081b3ce581fe01a73b8ecb2dfccd1a375
CJC0512 <ehdchs2006@gmail.com>
2024-03-26 15:46:01 +0900

    [학습] vue_bootstrap

---
+++ 09_vue_bootstrap/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+ <div class="mt-3">
+      <h6 class="text-center">Center alignment</h6>
+      <b-pagination v-model="currentPage" :total-rows="rows" align="center"></b-pagination>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+        rows: 100,
+        currentPage: 3
+      }
+    }
+  }
+</script>
